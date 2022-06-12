d053b0f9ec708433df93a836bf31adc581c10b1d
xavier9909 <sm798207@gmail.com>
2022-06-12 22:16:21 +0530

    w2d4

---
+++ w2d4/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import styles from './App.module.css'
+function App() {
+  return (
+    <div className={styles.App}>
+        <h1>hello</h1>
+    </div>
+  );
+}
+
+export default App;
